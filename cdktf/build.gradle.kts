e2a0b4085e5bb90c9e84c8bf1389f29a30caba47
tecchu11 <agdjmp.ajtt@gmail.com>
2022-08-07 18:54:50 +0900

    Manage versions by version catalog

--- cdktf/build.gradle.kts
+++ cdktf/build.gradle.kts
@@ -7,9 +7,7 @@ group = "org.example"
 version = "0.1"
 
 dependencies {
-    implementation("com.hashicorp:cdktf:0.12.0")
-    implementation("software.constructs:constructs:10.1.69")
-    implementation("com.hashicorp:cdktf-provider-docker:2.0.14")
+    implementation(libs.bundles.cdktf.docker)
 
     testImplementation(kotlin("test"))
 }
